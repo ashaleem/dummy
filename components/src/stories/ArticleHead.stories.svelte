a332dbad06e13aa2681f99ff69ffa2d16d397ac2
Frederick O'Brien <frederik.obrien@gmail.com>
2024-12-15 23:34:31 +0000

    Dummy UI story

---
+++ components/src/stories/ArticleHead.stories.svelte
@@ -0,0 +1,23 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import ArticleHead from '../lib/components/articles/ArticleHead.svelte';
+
+	const { Story } = defineMeta({
+		title: 'Articles/Article Head',
+		component: ArticleHead,
+		tags: ['autodocs'],
+		args: {
+			title: 'Man bites dog',
+			description: 'Park goers were shocked to see a game of fetch go south',
+			publicationDate: '2021-09-01',
+			authors: [
+				{
+					id: 'john-doe',
+					name: 'John Doe'
+				}
+			]
+		}
+	});
+</script>
+
+<Story name="Default" />
