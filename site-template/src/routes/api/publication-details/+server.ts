9982fc72f05d284b9a7f50449413232762462083
Frederick O'Brien <frederik.obrien@gmail.com>
2025-02-26 22:08:08 +0000

    Partially wire up customisable site details

--- site-template/src/routes/api/publication-details/+server.ts
+++ site-template/src/routes/api/publication-details/+server.ts
@@ -1,6 +1,57 @@
-import { PUBLICATION_DETAILS } from "$lib/constants";
+import type { PublicationDetails } from "@gonzo-engineering/libs";
+import publicationDetails from "../../../data/site-details.json";
 import { json } from "@sveltejs/kit";
 
 export async function GET() {
-  return json(PUBLICATION_DETAILS);
+  const hydratedDetails: PublicationDetails = {
+    ...publicationDetails,
+    sections: [
+      {
+        label: "Old Town",
+        slug: "old-town",
+      },
+      {
+        label: "New Town",
+        slug: "new-town",
+      },
+      {
+        label: "Business District",
+        slug: "business-district",
+      },
+    ],
+    navigation: {
+      header: [
+        {
+          label: "Old Town",
+          url: "/old-town",
+          subNav: [
+            {
+              label: "Council",
+              url: "/tags/council",
+            },
+            {
+              label: "Planning",
+              url: "/tags/planning",
+            },
+          ],
+        },
+        {
+          label: "New Town",
+          url: "/new-town",
+        },
+        {
+          label: "Business District",
+          url: "/business-district",
+          subNav: [
+            {
+              label: "Culture",
+              url: "/tags/culture",
+            },
+          ],
+        },
+      ],
+      footer: [],
+    },
+  };
+  return json(hydratedDetails);
 }
