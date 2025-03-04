f6b4e41b862c085dc34b23f36fbb45a4de98b908
Frederick O'Brien <frederik.obrien@gmail.com>
2025-03-04 23:15:59 +0000

    Sections and nav links in CMS

--- site-template/src/routes/api/publication-details/+server.ts
+++ site-template/src/routes/api/publication-details/+server.ts
@@ -1,56 +1,14 @@
 import type { PublicationDetails } from "@gonzo-engineering/libs";
 import publicationDetails from "../../../data/site-details.json";
+import siteNavigation from "../../../data/navigation.json";
 import { json } from "@sveltejs/kit";
 
 export async function GET() {
   const hydratedDetails: PublicationDetails = {
     ...publicationDetails,
-    sections: [
-      {
-        label: "Old Town",
-        slug: "old-town",
-      },
-      {
-        label: "New Town",
-        slug: "new-town",
-      },
-      {
-        label: "Business District",
-        slug: "business-district",
-      },
-    ],
     navigation: {
-      header: [
-        {
-          label: "Old Town",
-          url: "/old-town",
-          subNav: [
-            {
-              label: "Council",
-              url: "/tags/council",
-            },
-            {
-              label: "Planning",
-              url: "/tags/planning",
-            },
-          ],
-        },
-        {
-          label: "New Town",
-          url: "/new-town",
-        },
-        {
-          label: "Business District",
-          url: "/business-district",
-          subNav: [
-            {
-              label: "Culture",
-              url: "/tags/culture",
-            },
-          ],
-        },
-      ],
-      footer: [],
+      header: siteNavigation.header ? siteNavigation.header : [],
+      footer: siteNavigation.footer ? siteNavigation.footer : [],
     },
   };
   return json(hydratedDetails);
